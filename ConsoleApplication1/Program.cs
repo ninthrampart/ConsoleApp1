c0346f93c6e12ed039dbd48b345341e1269d79d7
Chudakov Dmitrii <chudakov@gmail.com>
2015-08-22 22:26:54 +0300

    for youtrack 1

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi!");
+            Console.WriteLine("Hello, world!!!!");
             Console.ReadKey();
         }
     }
