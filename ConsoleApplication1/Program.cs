0706f9fd75c3d18109324322934a38229dea4922
Chudakov Dmitrii <chudakov@gmail.com>
2015-08-22 23:11:41 +0300

    #TEST-1 Fixed

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world1234!!!!1234");
+            Console.WriteLine("111 Hello, world1234!!!!1234");
             Console.ReadKey();
         }
     }
